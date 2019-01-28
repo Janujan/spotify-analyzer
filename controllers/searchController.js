4d73873db6064e1b2c0d24a1cbd6d78c943b7212
Janujan <janujan.selva@gmail.com>
2019-01-28 00:26:18 -0500

    integrating spotify api

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -1,4 +1,5 @@
 var url = require('url');
+var SpotifyWebApi = require('spotify-web-api-node');
 
 exports.index = function(req,res){
     res.render('index', {title: 'Spotify Playlist Analyzer'})
@@ -16,6 +17,48 @@ exports.post_index = function(req, res){
 };
 exports.results = function(req, res) {
     var passedVariable = req.query.playlist;
-    console.log('recieved data ' + passedVariable);
-    res.render('results', {title: 'Results'});
+    var results = [];
+    var spotifyApi = new SpotifyWebApi({
+        clientId: 'a1d89dc9d68c43d095e3a6d44fbfc76a',
+        clientSecret: 'bd17f8a23a3c45a18bfa1b593cd0977e',
+        redirectUri: 'http://www.example.com/callback',
+      });
+    // Retrieve an access token
+    spotifyApi
+    .clientCredentialsGrant()
+    .then(function(data) {
+        // Set the access token on the API object so that it's used in all future requests
+        spotifyApi.setAccessToken(data.body['access_token']);
+        // Get the most popular tracks by David Bowie in Great Britain
+        return spotifyApi.getPlaylistTracks(passedVariable, {
+            offset: 0,
+            limit: 20,
+            fields: 'items'
+        });
+        //return spotifyApi.getArtistTopTracks('0oSGxfWSnnOXhD2fKuz2Gy', 'GB');
+
+    })
+    .then(function(data) {
+        console.log(data.body.items)        
+        data.body.items.forEach(function(track, index) {
+            results.push(track.track);
+            console.log(
+              index +
+                1 +
+                '. ' +
+                track.track.name +
+                ' (id: ' +
+                track.track.id +
+                ')'
+            );
+        });
+        //console.log(results)
+
+        console.log(results)
+        console.log('recieved data ' + passedVariable);
+        res.render('results', {title: 'Results', data: results});
+    })
+    .catch(function(err) {
+        console.log('Unfortunately, something has gone wrong.', err.message);
+    });
 };
